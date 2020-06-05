a2aca65984647979d059330e02b22010c511973c
Steve2955 <yanbec2000@gmail.com>
2020-06-05 08:35:23 +0000

    Deploying to gh-pages from  @ 66f63671acb6d8aede416dd5be1d0d391ab633f8 🚀

---
+++ precache-manifest.813e6c607bf2f7141722311b893b83a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260df6eff3c93ed49ebd08da47a9f8c0",
+    "url": "/simple-weight-tracker/Demo.jpg"
+  },
+  {
+    "revision": "7d2aacc106a18e5b5cac",
+    "url": "/simple-weight-tracker/css/app.5558c664.css"
+  },
+  {
+    "revision": "14f0e1d186f1993583bf0cf7549b213e",
+    "url": "/simple-weight-tracker/index.html"
+  },
+  {
+    "revision": "7d2aacc106a18e5b5cac",
+    "url": "/simple-weight-tracker/js/app.379f1f88.js"
+  },
+  {
+    "revision": "3b8eec3dc72d089ed88a",
+    "url": "/simple-weight-tracker/js/chunk-vendors.4ab03bc9.js"
+  },
+  {
+    "revision": "3d0d4a444a20a6b747d565be3c27ef0f",
+    "url": "/simple-weight-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simple-weight-tracker/robots.txt"
+  }
+]);
